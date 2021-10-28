76b803ec4819c67e6d7ff90c38c17ee4d491308f
Lio9 <Lio9@qq.com>
2021-10-28 11:18:46 +0800

    增加元组的笔记

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-list = ['Google', 'Runoob', 1997, 2000]
+tup = ('Google', 'Runoob', 1997, 2000)
 
-print("原始列表 : ", list)
-del list[2]
-print("删除第三个元素 : ", list)
+print(tup)
+del tup
+print("删除后的元组 tup : ")
+print(tup)
