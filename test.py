b8df141590d9de18439fa38661d16aed114b913f
Lio9 <Lio9@qq.com>
2021-10-28 10:57:28 +0800

    增加列表的笔记

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-x = 1
-print(f'{x+1}')   # Python 3.6
+list = ['Google', 'Runoob', 1997, 2000]
 
-x = 1
-print(f'{x+1=}')   # Python 3.8
+print("原始列表 : ", list)
+del list[2]
+print("删除第三个元素 : ", list)
